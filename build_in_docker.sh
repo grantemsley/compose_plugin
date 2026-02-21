91777b9ddc1ed8920dd025a4da73d23dda005811
Aleksei Bibus <alexelizard@gmail.com>
2026-02-21 22:57:50 +1000

    build(compose): Update compose version to 5.0.2
    ISSUE: 39

--- build_in_docker.sh
+++ build_in_docker.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
-[ -z "$COMPOSE_VERSION" ] && COMPOSE_VERSION=2.40.3
-[ -z "$COMPOSE_SWITCH_VERSION" ] && COMPOSE_SWITCH_VERSION=1.0.5
+[ -z "$COMPOSE_VERSION" ] && COMPOSE_VERSION=5.0.2
 [ -z "$ACE_VERSION" ] && ACE_VERSION=1.43.5
-docker run --rm --tmpfs /tmp -v $PWD/archive:/mnt/output:rw -e TZ="America/New_York" -e COMPOSE_VERSION=$COMPOSE_VERSION -e COMPOSE_SWITCH_VERSION=$COMPOSE_SWITCH_VERSION -e ACE_VERSION=$ACE_VERSION -e OUTPUT_FOLDER="/mnt/output" -v $PWD/source:/mnt/source:ro vbatts/slackware:latest /mnt/source/pkg_build.sh $UI_VERSION_LETTER
+docker run --rm --tmpfs /tmp \
+    -v $PWD/archive:/mnt/output:rw \
+    -e TZ="America/New_York" \
+    -e COMPOSE_VERSION=$COMPOSE_VERSION \
+    -e ACE_VERSION=$ACE_VERSION \
+    -e OUTPUT_FOLDER="/mnt/output" \
+    -v $PWD/source:/mnt/source:ro \
+    vbatts/slackware:latest \
+    /mnt/source/pkg_build.sh $UI_VERSION_LETTER
