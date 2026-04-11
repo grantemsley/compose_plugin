9d8230e473889be182422515b1cbb39c9982e67a
mstrhakr <mstrhakr@gmail.com>
2026-04-10 22:23:47 -0400

    fix(tests): update require path for Util.php in ImportWizardTest

--- tests/unit/ImportWizardTest.php
+++ tests/unit/ImportWizardTest.php
@@ -17,7 +17,7 @@ namespace ComposeManager\Tests;
 
 use PluginTests\TestCase;
 
-require_once '/usr/local/emhttp/plugins/compose.manager/php/util.php';
+require_once '/usr/local/emhttp/plugins/compose.manager/include/Util.php';
 
 class ImportWizardTest extends TestCase
 {
