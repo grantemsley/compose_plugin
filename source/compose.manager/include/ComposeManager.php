0d51c4aa0d6c82bdc959c0b54bba0b21fc2cc67b
mstrhakr <mstrhakr@gmail.com>
2026-06-08 23:13:16 -0400

    fix(docker.versions): conditionally load external styles for Docker versions if available

--- source/compose.manager/include/ComposeManager.php
+++ source/compose.manager/include/ComposeManager.php
@@ -289,6 +289,9 @@ $acePath = file_exists('/usr/local/emhttp/plugins/dynamix/javascript/ace/ace.js'
 <script src="<?php autov('/plugins/compose.manager/javascript/common.js'); ?>" type="text/javascript"></script>
 <script src="<?php autov('/plugins/compose.manager/javascript/composeSortable.js'); ?>" type="text/javascript"></script>
 <script src="<?php autov('/plugins/compose.manager/javascript/composeStackUtils.js'); ?>" type="text/javascript"></script>
+<?php if (file_exists('/usr/local/emhttp/plugins/docker.versions/styles/styles.css')): ?>
+<link type="text/css" rel="stylesheet" href="<?php autov('/plugins/docker.versions/styles/styles.css'); ?>">
+<?php endif; ?>
 <?php if (file_exists('/usr/local/emhttp/plugins/docker.versions/scripts/changelog.js')): ?>
 <script src="<?php autov('/plugins/docker.versions/scripts/changelog.js'); ?>" type="text/javascript"></script>
 <?php endif; ?>
